bf0cc7b0a84cac0b4a5d5e996077f71dd99df398
holybanguda <yjkwon0826@naver.com>
2026-05-23 18:42:25 +0900

    made search function searchQuery = 검색어 checkWritten = 필기시험 체크박스 checkPractical = 실기시험 체크박스

--- app/src/main/java/com/tp/licenseList/LicenseDao.java
+++ app/src/main/java/com/tp/licenseList/LicenseDao.java
@@ -8,4 +8,19 @@ import java.util.List;
 public interface LicenseDao {
     @Query("SELECT * FROM licenseList")
     List<License> getAll(); // 150개 데이터를 한 번에 다 가져오는 함수
+
+    @Query("SELECT * FROM licenseList WHERE " +
+            "(name LIKE '%' || :searchQuery || '%' OR " +
+            "category LIKE '%' || :searchQuery || '%' OR " +
+            "qualification LIKE '%' || :searchQuery || '%') " +
+
+
+            "AND (" +
+
+            "(:checkWritten = 0 AND :checkPractical = 0) OR " +
+            "(:checkWritten = 1 AND :checkPractical = 1) OR " +
+            "(:checkWritten = 1 AND :checkPractical = 0 AND form LIKE '%필기%') OR " +
+            "(:checkWritten = 0 AND :checkPractical = 1 AND form LIKE '%실기%')" +
+            ")")
+    List<License> searchLicense(String searchQuery, int checkWritten, int checkPractical);
 }
